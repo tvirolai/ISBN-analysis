97d9376f3a1b67f656c59ea665db217e3ffcb8bd
Tuomo Virolainen <tuomo.virolainen@helsinki.fi>
2015-11-03 15:58:32 +0200

    ...

--- haku.js
+++ haku.js
@@ -5,14 +5,20 @@ var file = process.argv[2];
 var readline = require('readline');
 var fs = require('fs');
 // var serialOrMonograph = require('./serialOrMonograph.js');
-var isbnChecker = require('./lib/isbnChecker.js');
-
-var processLine = require('./lib/processLine.js');
 
 if (!file) {
   console.log('Usage: node haku.js inputfile');
   process.exit();
+} else {
+  var dirStuff = fs.readdirSync('./output/');
+  if (dirStuff.length > 0) {
+    console.log('Output directory (' + __dirname + '/output)' + ' not empty, aborting.');
+    process.exit();
+  }
 }
+
+var isbnChecker = require('./lib/isbnChecker.js');
+var processLine = require('./lib/processLine.js');
 
 var rl = readline.createInterface({
   input: fs.createReadStream(file)
